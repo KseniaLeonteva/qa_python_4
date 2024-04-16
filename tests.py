652876b6a3815f924af9cdf5a44c450c55b170d2
Ksenia Leonteva <leontjeva.k@yandex.ru>
2024-04-16 19:41:59 +0300

    commit with test for add_new_book

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -17,9 +19,39 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert collector.get_books_genre() == {'Гордость и предубеждение и зомби' : '',
-                                              'Что делать, если ваш кот хочет вас убить' : ''}
+        assert collector.get_books_genre() == {'Гордость и предубеждение и зомби': '',
+                                              'Что делать, если ваш кот хочет вас убить': ''}
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    #добавляем книгу с названием в 41 символ
+    #def test_add_new_book_name_41_symbols(self):
+        #collector = BooksCollector()
+
+       #collector.add_new_book('Удивительное путешествие Нильса Хольгерсс')
+        #assert collector.get_books_genre() == {}
+
+    #добавляем книгу без названия
+    #def test_add_new_book_without_name(self):
+        #collector = BooksCollector()
+
+        #collector.add_new_book('')
+        #assert collector.get_books_genre() == {}
+
+    #добавляем уже добавленную книгу
+    def test_add_new_book_already_added_book(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение')
+        collector.add_new_book('Гордость и предубеждение')
+
+        assert collector.get_books_genre() == {'Гордость и предубеждение': ''}
+
+    #не добавляются книги вне диапазона(название)
+    @pytest.mark.parametrize('name', ['', 'Удивительное путешествие Нильса Хольгерсс'])
+    def test_add_new_book(self, name):
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+        assert collector.get_books_genre() == {}
