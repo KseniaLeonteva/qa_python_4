fcb33302b347897294feada7d51634ca857087b3
Ksenia Leonteva <leontjeva.k@yandex.ru>
2024-04-18 14:26:31 +0300

    commit last test

--- tests.py
+++ tests.py
@@ -36,12 +36,12 @@ class TestBooksCollector:
         assert len(books_collection.get_books_genre()) == 0
 
     #добавляются книги из дипазона(название)
-    #@pytest.mark.parametrize('name', ['К югу от границы',
-                                      #'Любовь — как роза, красива, но шипы больны',
-                                      #'Я'])
-    #def test_add_new_book_name_in_the_range(self, name, books_collection):
-        #books_collection.add_new_book(name)
-        #assert name in books_collection.books_genre
+    @pytest.mark.parametrize('name', ['К югу от границы',
+                                      'Любовь как роза, красива, но шипы больны',
+                                      'Я'])
+    def test_add_new_book_name_in_the_range(self, name, books_collection):
+        books_collection.add_new_book(name)
+        assert name in books_collection.get_books_genre()
 
     #устанавливаем жанр книге
     def test_set_book_genre_to_existing_book(self):
@@ -81,7 +81,7 @@ class TestBooksCollector:
 
         collector.add_new_book(name)
         collector.set_book_genre(name, genre)
-        assert collector.get_book_genre(name)  == genre
+        assert collector.get_book_genre(name) == genre
 
     #вывод книг по жанру
     def test_get_books_with_specific_genre_by_genre(self, my_books_collection):
@@ -94,3 +94,44 @@ class TestBooksCollector:
     #вывод дестких книг
     def test_get_books_for_children(self, my_books_collection):
         assert len(my_books_collection.get_books_for_children()) == 3 and my_books_collection.get_books_for_children() == ['Симбиоз', 'Гадкий Я', 'Ревизор']
+
+    #добавить в избранное
+    def test_add_book_in_favorites_not_added_in_favorites_book(self, my_books_collection):
+        my_books_collection.add_book_in_favorites('Симбиоз')
+
+        assert 'Симбиоз' in my_books_collection.get_list_of_favorites_books() and len(my_books_collection.get_list_of_favorites_books()) == 1
+
+    #добавить в избранное одну и ту же книгу
+    def test_add_book_in_favorites_added_in_favorites_book(self, my_books_collection):
+        my_books_collection.add_book_in_favorites('Симбиоз')
+        my_books_collection.add_book_in_favorites('Симбиоз')
+
+        assert 'Симбиоз' in my_books_collection.get_list_of_favorites_books() and len(my_books_collection.get_list_of_favorites_books()) == 1
+
+    #добавить в избранное книгу не из books_genre
+    def test_add_book_in_favorites_not_added_dict_book(self, my_books_collection):
+        book = 'Идиот'
+        my_books_collection.add_book_in_favorites(book)
+
+        assert len(my_books_collection.get_list_of_favorites_books()) == 0
+
+    #удалить из избранного
+    def test_delete_book_from_favorites(self, my_books_collection):
+        my_books_collection.add_book_in_favorites('Симбиоз')
+        my_books_collection.delete_book_from_favorites('Симбиоз')
+
+        assert len(my_books_collection.get_list_of_favorites_books()) == 0
+
+    # удалить из избранного книгу не из избранного
+    def test_delete_book_from_favorites_deleted_book(self, my_books_collection):
+        my_books_collection.add_book_in_favorites('Симбиоз')
+        my_books_collection.delete_book_from_favorites('Симбиоз')
+
+        assert len(my_books_collection.get_list_of_favorites_books()) == 0 and 'Симбиоз' not in my_books_collection.get_list_of_favorites_books()
+
+    #получить список избранных книг
+    def test_get_list_of_favorites_books(self, my_books_collection):
+        my_books_collection.add_book_in_favorites('Симбиоз')
+        my_books_collection.add_book_in_favorites('Гадкий Я')
+
+        assert my_books_collection.get_list_of_favorites_books() == ['Симбиоз', 'Гадкий Я']
