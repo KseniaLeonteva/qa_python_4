c6167f965b422a8f8c78eb3c2eb3b00ad5b8f595
Ksenia Leonteva <leontjeva.k@yandex.ru>
2024-04-18 16:55:09 +0300

    commit by PEP8

--- conftest.py
+++ conftest.py
@@ -5,7 +5,6 @@ from main import BooksCollector
 @pytest.fixture
 def books_collection():
     books_collection = BooksCollector()
-
     return books_collection
 
 
@@ -16,9 +15,7 @@ def my_books_collection(books_collection):
     genre = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
     for i in range(5):
         my_collection.add_new_book(books[i])
-
     for i in range(5):
         my_collection.set_book_genre(books[i], genre[i])
-
     return my_collection
 
